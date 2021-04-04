7f8c4618f9b663b243f8bd4ad52fb03a7a240093
snetsrac <dev@chriscarstens.com>
2021-04-04 21:49:33 +0000

    Add campground delete route

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ app.get('/campgrounds/:id', async (req, res) => {
   res.render('campgrounds/show', { campground });
 });
 
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect('/campgrounds');
+});
+
 app.listen(3000, () => {
   console.log('Listening on 3000.');
 });
