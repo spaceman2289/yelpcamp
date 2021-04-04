72153a15c20b3ed4e9926ef1937a4eaa8a996ae1
snetsrac <dev@chriscarstens.com>
2021-04-04 20:29:38 +0000

    Add campground show route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const { nextTick } = require('process');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -25,6 +26,11 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find();
   res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
 });
 
 app.listen(3000, () => {
