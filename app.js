9ac7e0959cb1f921ffe14a46a99d3c26275115f0
snetsrac <dev@chriscarstens.com>
2021-04-04 18:04:01 +0000

    Add database connection

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.on('open', () => { console.log('Database connected.'); });
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const myCampground = new Campground({
+    title: 'Dry Gulch',
+    description: 'It\'s dry and gulchy.',
+    price: 'A buck two-eighty fifty.',
+    location: 'Tuba City, NM'
+  });
+  await myCampground.save();
+  res.send(myCampground);
 });
 
 app.listen(3000, () => {
