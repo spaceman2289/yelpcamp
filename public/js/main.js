715cbbca07232e2ef5042a27453c6fc6b9450514
snetsrac <dev@chriscarstens.com>
2021-04-09 21:40:00 +0000

    Add rating stars

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,41 @@
+const dates = document.querySelectorAll('.date');
+
+dates.forEach((date) => {
+  date.textContent = new Date(date.textContent).toLocaleString();
+});
+
+
+const reviewForm = document.querySelector('#reviewForm');
+
+if (reviewForm) {
+  const stars = document.querySelectorAll('#reviewForm input[type="radio"][name="review[rating]"]')
+
+  stars.setValidity = (string) => {
+    stars.forEach((star) => {
+      star.setCustomValidity(string);
+      star.checkValidity();
+    });
+  };
+
+  stars.forEach((star) => {
+    star.addEventListener('click', () => {
+      if (stars[0].checked) {
+        stars.setValidity('invalid');
+      } else {
+        stars.setValidity('');
+      }
+    });
+  });
+
+  reviewForm.addEventListener('submit', () => {
+    if (stars[0].checked) {
+      stars.setValidity('invalid');
+    } else {
+      stars.setValidity('');
+    }
+  });
+}
+
 const forms = document.querySelectorAll('.needs-validation')
     
 forms.forEach((form) => {
@@ -9,10 +47,4 @@ forms.forEach((form) => {
 
     form.classList.add('was-validated')
   });
-});
-
-const dates = document.querySelectorAll('.date');
-
-dates.forEach((date) => {
-  date.textContent = new Date(date.textContent).toLocaleString();
 });
