a75547e4c2023bc65c5a101b520d6c9ec5ec8a52
snetsrac <dev@chriscarstens.com>
2021-04-13 15:57:56 +0000

    Add helmet

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const path = require('path');
+const createError = require('http-errors');
 const engine = require('ejs-mate');
 const express = require('express');
-const createError = require('http-errors');
 const flash = require('connect-flash');
+const helmet = require('helmet');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -36,12 +37,40 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      'connect-src': [
+        "https://api.mapbox.com",
+        "https://events.mapbox.com"
+      ],
+      'img-src': [
+        "'self'",
+        "data:",
+        "https://res.cloudinary.com/da8gaqzfc/",
+        "https://source.unsplash.com",
+        "https://images.unsplash.com"
+      ],
+      'script-src': [
+        "'self'",
+        "'unsafe-inline'",
+        "blob:",
+        "https://cdn.jsdelivr.net",
+        "https://api.mapbox.com"
+      ]
+    }
+  }
+}));
+
 app.use(session({
   secret: 'super secret',
   resave: false,
   saveUninitialized: true,
   cookie: {
+    name: '_yc',
     httpOnly: true,
+    //secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }));
