7e22e4d0d28598802473e976d2995818ada9e264
snetsrac <dev@chriscarstens.com>
2021-04-14 20:27:10 +0000

    Add http logging

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const mongoSanitize = require('express-mongo-sanitize');
+const morgan = require('morgan');
 const passport = require('passport');
 const { session, helmet, setLocals, returnTo } = require('./middleware');
 const { User } = require('./models');
@@ -20,16 +21,16 @@ app.use(helmet);
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') app.use(morgan('combined'));
 
 app.use(session);
-
+app.use(flash());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(flash());
 app.use(returnTo);
 app.use(setLocals);
 
